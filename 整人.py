2b4323c2137e39417decf2402c69ba0fff3b60ad
heqixuan001 <65164324+heqixuan001@users.noreply.github.com>
2020-06-12 22:14:34 +0800

    Update 整人.py

--- 整人.py
+++ 整人.py
@@ -4,6 +4,7 @@ import tkinter as tk
 a = tk.Tk()
 a.geometry('400x400')
 
+
 def enter():
     gui.hotkey('enter')
 
@@ -19,9 +20,9 @@ def hello():
         enter()
 
 
-tk.Label(a, text="把发送改为enter",bg='green').pack()
-tk.Label(a, text="请打开qq联系人",bg='yellow').pack()
-tk.Button(a, text="点我发送1000条信息", command=hello,bg='red').pack()
+tk.Label(a, text="把发送改为enter", bg='green').pack()
+tk.Label(a, text="请打开qq联系人", bg='yellow').pack()
+tk.Button(a, text="点我发送1000条信息", command=hello, bg='red').pack()
 
 
 a.mainloop()
